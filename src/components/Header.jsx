22390783a82ad4630e387e6d7f2522899b806cfb
Naveenchand06 <naveenchand0606@gmail.com>
2022-04-18 12:00:39 +0530

    basic base setup

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { GrAddCircle } from "react-icons/gr";
+
+function Header() {
+  return (
+    <div className="header-container">
+      <h2>Todo App</h2>
+      <GrAddCircle />
+    </div>
+  );
+}
+
+export default Header;
